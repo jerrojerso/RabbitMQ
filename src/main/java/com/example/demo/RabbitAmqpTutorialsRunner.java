1cfe25d38ebb58e726f87126b2a10964b4de376c
Jeremy Muller <jeremy-muller@student.fontys.nl>
2021-04-12 23:02:37 +0200

    Ja ff alles erin flikkeren

---
+++ src/main/java/com/example/demo/RabbitAmqpTutorialsRunner.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class RabbitAmqpTutorialsRunner implements CommandLineRunner {
+
+    @Value("${tutorial.client.duration:0}")
+    private int duration;
+
+    @Autowired
+    private ConfigurableApplicationContext ctx;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
